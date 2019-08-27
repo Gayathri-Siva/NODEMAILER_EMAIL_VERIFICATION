7f777ca96528211a0bf307c39de9fbbccefe9b14
Gayathri~brittany <44465017+gayathribrittany@users.noreply.github.com>
2019-08-27 10:14:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express'),
+    path = require('path'),
+    nodeMailer = require('nodemailer'),
+    bodyParser = require('body-parser');
+
+    var app = express();
+    app.set('view engine', 'ejs');
+    app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    var port = 3000;
+    app.get('/', function (req, res) {
+      res.render('index');
+    });
+    app.post('/send-email', function (req, res) {
+      let transporter = nodeMailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 465,
+          secure: true,
+          auth: {
+              user: 'gayathri.testing2019@gmail.com',
+              pass: '************'
+          }
+      });
+      let mailOptions = {
+          from: 'gayathri.testing2019@gmail.com', // sender address
+          to: req.body.to, // list of receivers
+          subject: "nodemailer", // Subject line
+          text: "how are you", // plain text body
+          html: '<b>NodeJS Email Tutorial</b>' // html body
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message %s sent: %s', info.messageId, info.response);
+              res.render('index');
+          });
+      });
+          app.listen(port, function(){
+            console.log('Server is running at port: ',port);
+          });
